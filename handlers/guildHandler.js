db1ba7be2bd1625ca78fc14c8696f722dd6e6aea
Aetheryx <zain1@live.nl>
2017-05-05 15:23:45 +0200

    Fixed lethal bug, request -> sagent

--- handlers/guildHandler.js
+++ handlers/guildHandler.js
@@ -9,7 +9,7 @@ exports.create = function(guild) {
     guild.defaultChannel.send(`Hi, I'm ${client.user.username} and I give you the possibility to set reminders and view or delete them. To see a list of my commands, send \`${prefixdb[guild.id]}help\`.\nFeel free to dm Aetheryx#2222 for any questions or concerns!`);
 
     if (!settings.keys.botspw.includes('API key'))
-        request
+        sagent
         .post('https://bots.discord.pw/api/bots/' + client.user.id + '/stats')
         .set('Authorization', settings.keys.botspw)
         .send({
@@ -32,11 +32,11 @@ exports.delete = function(guild) {
     delete prefixdb[guild.id];
 
     fs.writeFile('./storage/prefixdb.json', JSON.stringify(prefixdb, '', '\t'), (err) => {
-        if (err) return console.log(Date() + ' createGuildHandler error: ' + err)
+        if (err) return console.log(Date() + ' deleteGuildHandler error: ' + err)
     });
 
     if (!settings.keys.botspw.includes('API key'))
-        request
+        sagent
         .post('https://bots.discord.pw/api/bots/' + client.user.id + '/stats')
         .set('Authorization', settings.keys.botspw)
         .send({
