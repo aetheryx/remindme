271a729b13f14723c73eaf75e9bca75802848ef3
Aetheryx <zain1@live.nl>
2017-07-13 23:14:10 +0200

    Interval set to 2 seconds, now runs once at boot aswell

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,12 +1,16 @@
-setInterval(() => {
+updateValues();
+setInterval(updateValues, 2000);
+
+function updateValues () {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            const array = ['guilds', 'channels', 'users'];
+            const stats = JSON.parse(xhttp.responseText);
+            const array = Object.keys(stats);
             for (const value in array)
-                document.getElementById(array[value]).innerHTML = JSON.parse(xhttp.responseText)[array[value]];
+                document.getElementById(array[value]).innerHTML = stats[array[value]];
         }
     };
     xhttp.open('GET', '/api/stats', true);
     xhttp.send();
-}, 10000);
+}
