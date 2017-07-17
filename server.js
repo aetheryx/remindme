06eb3117132c1ba75ad5ee86634681ae696b70a3
Aetheryx <zain1@live.nl>
2017-07-18 00:43:56 +0200

    Added RAM Usage to stats endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const os = require('os');
 
 module.exports = function () {
     app.listen(80, () => {
@@ -12,7 +13,10 @@ module.exports = function () {
         res.send(JSON.stringify({
             guilds: this.guilds.size,
             channels: `${this.channels.filter(c => c.type === 'voice').size} voice, ${this.channels.filter(c => c.type === 'text').size} text (${this.channels.size} total)`,
-            users: `${this.guilds.map(g => parseInt(g.memberCount)).reduce((a, b) => { return a + b; })} (${this.users.size} online)`}));
+            users: `${this.guilds.map(g => parseInt(g.memberCount)).reduce((a, b) => { return a + b; })} (${this.users.size} online)`,
+            ram: `${(process.memoryUsage().rss / 1048576).toFixed()}MB/${(os.totalmem() > 1073741824 ? `${(os.totalmem() / 1073741824).toFixed(1)}GB` : `${(os.totalmem() / 1048576).toFixed()}MB`)}
+            (${(process.memoryUsage().rss / os.totalmem() * 100).toFixed(2)}%), ${(os.freemem() > 1073741824 ? `${(os.freemem() / 1073741824).toFixed(1)}GB` : `${(os.freemem() / 1048576).toFixed()}MB`)} free on server`}
+        ));
     });
 
     app.get('/api/uptime', (req, res) => {
