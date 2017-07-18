7689180d58428bb261cb2e5df6f722d9a89bf577
Aetheryx <zain1@live.nl>
2017-07-19 00:11:59 +0200

    Added date to ready log, adjusted for 2 spooky DM channels

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const os = promisifyAll(require('os-utils'));
 
 module.exports = function () {
     app.listen(80, () => {
-        console.log('Listening on port 80.');
+        console.log(`${Date().toString().split(' ').slice(1, 5).join(' ')} Listening on port 80.`);
     });
 
     app.use(express.static('dashboard'));
@@ -13,7 +13,7 @@ module.exports = function () {
     app.get('/api/stats', async (req, res) => {
         res.send(JSON.stringify({
             guilds: this.guilds.size,
-            channels: `${this.channels.filter(c => c.type === 'voice').size} voice, ${this.channels.filter(c => c.type === 'text').size} text (${this.channels.size} total)`,
+            channels: `${this.channels.filter(c => c.type === 'voice').size} voice, ${this.channels.filter(c => c.type === 'text').size} text (${this.channels.filter(c => c.type === 'text' || c.type === 'voice').size} total)`,
             users: `${this.guilds.map(g => parseInt(g.memberCount)).reduce((a, b) => { return a + b; })} (${this.users.size} online)`,
             ram: `${(process.memoryUsage().rss / (1024 * 1024)).toFixed()}MB/${(os.totalmem() > 1000 ? `${(os.totalmem() / 1000).toFixed(1)}GB` : `${(os.totalmem()).toFixed()}MB`)}
             (${(process.memoryUsage().rss / (os.totalmem() * 1024 * 1024) * 100).toFixed(2)}%), ${(os.freemem() > 1024 ? `${(os.freemem() / 1024).toFixed(1)}GB` : `${(os.freemem()).toFixed()}MB`)} free on server`, // very big mess. Fix later. os-util uses SI, process.memoryUsage() uses binary.
