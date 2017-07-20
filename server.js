4e4f7b9624320c9baa488e393aff79d22bd10e8f
Aetheryx <zain1@live.nl>
2017-07-20 02:15:41 +0200

    Removed CPU averages, added port variable

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const os = promisifyAll(require('os-utils'));
+const port = os.platform() === 'win32' ? 42069 : 80; // My router doesn't like it when I host something on port 80 :c
 
 
 module.exports = function () {
-    app.listen(80, () => {
-        console.log(`${Date().toString().split(' ').slice(1, 5).join(' ')} Listening on port 80.`);
+    app.listen(port, () => {
+        console.log(`${Date().toString().split(' ').slice(1, 5).join(' ')} Listening on port ${port}.`);
     });
 
     app.use(express.static('dashboard'));
@@ -17,7 +18,7 @@ module.exports = function () {
             users: `${this.guilds.map(g => parseInt(g.memberCount)).reduce((a, b) => { return a + b; })} (${this.users.size} online)`,
             ram: `${(process.memoryUsage().rss / (1024 * 1024)).toFixed()}MB/${(os.totalmem() > 1000 ? `${(os.totalmem() / 1000).toFixed(1)}GB` : `${(os.totalmem()).toFixed()}MB`)}
             (${(process.memoryUsage().rss / (os.totalmem() * 1024 * 1024) * 100).toFixed(2)}%), ${(os.freemem() > 1024 ? `${(os.freemem() / 1024).toFixed(1)}GB` : `${(os.freemem()).toFixed()}MB`)} free on server`, // very big mess. Fix later. os-util uses SI, process.memoryUsage() uses binary.
-            cpu: `${(await os.cpuUsageAsync() * 100).toFixed(2)}% (1/5/15 min avg: ${os.loadavg(1).toFixed(2)}/${os.loadavg(5).toFixed(2)}/${os.loadavg(15).toFixed(2)})`}
+            cpu: `${(await os.cpuUsageAsync() * 100).toFixed(2)}%`}
         ));
     });
 
