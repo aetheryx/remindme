7338068eae0b3ccf9e8c386330a349d660b14eab
Aetheryx <zain1@live.nl>
2017-08-10 15:19:03 +0200

    Removed seperate-dashboard
    Not really needed anymore. 
    For anyone wondering, that *was* a test bot token, and it is already expired.

--- seperate-dashboard.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const dashboard = require('./server.js').bind(client);
-
-client.login('MjkwOTQ3OTcwNDU3Nzk2NjA4.DEw7Kw.WZ_6EkA2YSmTIK55KZ1L2M2Gb1A');
-
-client.on('ready', dashboard);
