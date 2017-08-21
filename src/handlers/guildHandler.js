99e548393f2e5943a683a15a1c701fb72f3259cb
Aetheryx <zain1@live.nl>
2017-08-22 01:45:59 +0200

    Add guildHandlers

--- src/handlers/guildHandler.js
+++ src/handlers/guildHandler.js
@@ -1,40 +1,30 @@
-/* eslint-disable */ // (TO BE REWORKED SOON)
-
 const snekfetch = require('snekfetch');
-const fs        = require('fs');
 
-exports.create = (guild) => {
-    prefixdb[guild.id] = settings.defaultPrefix;
-    fs.writeFile('./storage/prefixdb.json', JSON.stringify(prefixdb, '', '\t'), (err) => {
-        if (err)
-            return console.log(`${Date()} createGuildHandler error: ${err}`);
-    });
-    guild.defaultChannel.send(`Hi, I'm ${client.user.username} and I give you the possibility to set reminders and view or delete them. 
-    To see a list of my commands, send \`${prefixdb[guild.id]}help\`.\nFeel free to DM Aetheryx#2222 for any questions or concerns!`);
-    postStats();
+exports.create = (Bot, guild) => {
+    guild.owner.send(`Thanks for adding me to your server! To see a list of my commands, send \`${Bot.config.defaultPrefix}help\`.\nFeel free to DM Aetheryx#2222 for any questions or concerns!`);
+    postStats(Bot);
 };
 
-exports.delete = (guild) => {
-    delete prefixdb[guild.id];
-    fs.writeFile('./storage/prefixdb.json', JSON.stringify(prefixdb, '', '\t'), (err) => {
-        if (err)
-            return console.log(`${Date()} deleteGuildHandler error: ${err}`);
-    });
-    postStats();
+exports.delete = async (Bot, guild) => {
+    await Bot.db.run('DELETE FROM prefixes WHERE guildID = ?;', guild.id);
+    Bot.prefixes.delete(guild.id);
+    postStats(Bot);
 };
 
-function postStats () {
-    if (!settings.keys.botspw.includes('API key'))
+function postStats (Bot) {
+    if (Bot.config.keys.botspw) {
         snekfetch
-            .post(`https://bots.discord.pw/api/bots/${client.user.id}/stats`)
-            .set('Authorization', settings.keys.botspw)
-            .send({ server_count: client.guilds.size })
+            .post(`https://bots.discord.pw/api/bots/${Bot.client.user.id}/stats`)
+            .set('Authorization', Bot.config.keys.botspw)
+            .send({ server_count: Bot.client.guilds.size })
             .end();
+    }
 
-    if (!settings.keys.dbots.includes('API key'))
+    if (Bot.config.keys.dbots) {
         snekfetch
-            .post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
-            .set('Authorization', settings.keys.dbots)
-            .send({ server_count: client.guilds.size })
+            .post(`https://discordbots.org/api/bots/${Bot.client.user.id}/stats`)
+            .set('Authorization', Bot.config.keys.dbots)
+            .send({ server_count: Bot.client.guilds.size })
             .end();
+    }
 }
