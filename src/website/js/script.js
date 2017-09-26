73308f3f6cf9caccf7882db525aa22f75ec1ddd6
Aetheryx <zain1@live.nl>
2017-09-26 18:44:12 +0200

    Use jsonp to load commands

--- src/website/js/script.js
+++ src/website/js/script.js
@@ -2,57 +2,7 @@ var uptime = 0;
 var statsPage = true;
 var index = 3;
 
-const commands = [
-    {
-        Name: 'help',
-        Info: 'Returns a help message to get you started with RemindMeBot.',
-        Usage: 'r>help'
-    },
-    {
-        Name: 'remindme',
-        Info: 'Creates a reminder. Pass without args to start a guided tour.',
-        Usage: 'r>remindme [<time argument> "<message>"]',
-        Example: 'r>remindme 12 hours "Buy groceries"'
-    },
-    {
-        Name: 'list',
-        Info: ['Sends you your current reminders in DM.', 'Note: will send in channel if DMs are disabled.'],
-        Usage: 'r>list'
-    },
-    {
-        Name: 'clear',
-        Info: 'Starts a guided tour to clear all of your reminders.',
-        Usage: 'r>clear'
-    },
-    {
-        Name: 'forget',
-        Info: 'Starts a guided tour to forget one of your reminders.',
-        Usage: 'r>forget'
-    },
-    {
-        Name: 'prefix',
-        Info: 'Changes your prefix to the given argument.',
-        Usage: 'r>prefix <desired prefix>',
-        Example: 'r>prefix !!'
-    },
-    {
-        Name: 'invite',
-        Info: 'Returns an invite for RemindMeBot.',
-        Usage: 'r>invite'
-    },
-    {
-        Name: 'ping',
-        Info: '	Returns the Websocket ping to the API servers in ms.',
-        Usage: 'r>ping'
-    },
-    {
-        Name: 'stats',
-        Info: 'Returns information and statistics about RemindMeBot.',
-        Usage: 'r>stats'
-    }
-];
-
-function loadCommands () { // eslint-disable-line no-unused-vars
+function loadCommands (commands) { // eslint-disable-line no-unused-vars
     document.getElementById('commands').appendChild(document.createElement('br'));
     commands.forEach(command => {
         const card = document.createElement('div');
@@ -67,7 +17,7 @@ function loadCommands () { // eslint-disable-line no-unused-vars
                 const tag = document.createElement('kbd');
                 const text = document.createTextNode(command[commandKey]);
                 tag.appendChild(text);
-                card.appendChild(document.createTextNode(commandKey + ': '));
+                card.appendChild(document.createTextNode(`${commandKey}: `));
                 card.appendChild(tag);
             } else {
                 card.appendChild(document.createTextNode(`${commandKey}: ${command[commandKey]}`));
