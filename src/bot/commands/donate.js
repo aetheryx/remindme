20025031e438ad226696f295ad5fb01b7b6197c8
aetheryx <zain1@live.nl>
2017-12-31 05:08:02 +0100

    Break up string

--- src/bot/commands/donate.js
+++ src/bot/commands/donate.js
@@ -2,7 +2,11 @@
 // \n • Your name and a custom phrase on [RemindMeBot\'s website](http://remindmebot.xyz/) (At least $2.00)
 async function donateCommand (Bot, msg) {
   Bot.sendMessage(msg.channel.id, { embed: {
-    description: 'RemindMeBot will always remain a free service. However, if you would like to donate, you can do so [here](https://paypal.me/Aether2017) (PayPal) or to `18UNsWHGvskZSxKc6LvyaqwKk5FUf2af4D` (BTC).\n\nAs a donator, you will receive:\n • A `Donator` role on [RemindMeBot Support](https://discord.gg/Yphr6WG) (Any amount)\n • Your name and a custom phrase on this command (At least $1.00)\n • Lots of hugs & kisses from Aetheryx (Any amount :kissing_heart:)\n\u200b',
+    description: 'RemindMeBot will always remain a free service. However, if you would like to donate, you can do so [here](https://paypal.me/Aether2017) (PayPal) or to `18UNsWHGvskZSxKc6LvyaqwKk5FUf2af4D` (BTC).' + 
+    '\n\nAs a donator, you will receive:\n' +
+    ' • A `Donator` role on [RemindMeBot Support](https://discord.gg/Yphr6WG) (Any amount)\n' +
+    ' • Your name and a custom phrase on this command (At least $1.00)\n' +
+    ' • Lots of hugs & kisses from Aetheryx (Any amount :kissing_heart:)\n\u200b',
     fields: [
       { name: 'Donators so far', value: 'None! You should totally be the first.' }
     ],
