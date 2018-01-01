67ca4d956dea42787e94452751ca12cbc00def81
aetheryx <zain1@live.nl>
2018-01-01 04:13:00 +0100

    add clear command

---
+++ src/bot/commands/clear.js
@@ -0,0 +1,31 @@
+async function clearCommand (msg) {
+  const reminders = await this.db.getReminders(msg.author.id);
+  if (!reminders[0]) {
+    return 'You have no reminders set.';
+  }
+
+  this.sendMessage(msg.channel.id, `:warning: This will delete all of your reminders (${reminders.length} total)! Are you sure? (\`y\`/\`n\`)`);
+
+  // eslint-disable-next-line prefer-const
+  let [message, reason] = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 25000 });
+
+  if (!message[0] && reason === 'time') {
+    return 'Prompt timed out.';
+  }
+
+  message = message[0].content.toLowerCase();
+  if (message === 'y') {
+    await Promise.all(reminders.map(reminder => this.db.deleteReminder(reminder._id)));
+    return `:ballot_box_with_check: ${reminders.length} reminders cleared.`;
+  } else if (message.includes('clear')) {
+    return;
+  } else {
+    return 'Cancelled.';
+  }
+}
+
+module.exports = {
+  call: clearCommand,
+  name: 'clear',
+  description : 'Starts a guided tour to clear all of your reminders.'
+};
