f65eac463d8f6280289d4e16969072fab7fcf5d0
aetheryx <zain1@live.nl>
2018-01-02 04:45:57 +0100

    add status clock

--- src/bot/events/onceReady.js
+++ src/bot/events/onceReady.js
@@ -14,7 +14,7 @@ async function handleReminders () {
           this.sendMessage(reminder.ownerID, { content: `I tried to send this to <#${reminder.channelID}>, but I'm not allowed to speak there.`, embed }, true);
         }
       } else {
-        this.sendMessage(reminder.ownerID, { embed }, true);
+        this.sendMessage(reminder.ownerID, embed, true);
       }
     } catch (err) {
       this.log(err.stack, 'error');
@@ -33,10 +33,26 @@ async function handleReminders () {
   }
 }
 
+async function initStatusClock () {
+  let index = 0;
+  const statuses = [
+    'in %s guilds',
+    `${this.config.defaultPrefix}help`,
+    '@mention help'
+  ];
+  setInterval(function () {
+    index = (index + 1) % statuses.length;
+    this.editStatus('online', {
+      name: statuses[index].replace('%s', this.guilds.size)
+    });
+  }.bind(this.client), 20000);
+}
 
 async function onceReady () {
+  initStatusClock.call(this);
   setInterval(handleReminders.bind(this), this.config.tick || 3000);
 
+  this.startWebServer();
 }
 
 module.exports = onceReady;
