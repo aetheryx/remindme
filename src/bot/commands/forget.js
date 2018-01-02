47a2bf3a2381f23be5dc17b1e5857f56ae4e431e
aetheryx <zain1@live.nl>
2018-01-02 03:12:12 +0100

    add forget and list commands

---
+++ src/bot/commands/forget.js
@@ -0,0 +1,55 @@
+async function forgetCommand (msg) {
+  const reminders = await this.db.getReminders(msg.author.id);
+  if (!reminders[0]) {
+    return 'You have no reminders set.';
+  }
+  this.sendMessage(msg.channel.id, {
+    content: 'Here\'s a list of your current reminders:',
+    embed: {
+      title: 'Reminders',
+      description: reminders.map((r, i) => `[${i + 1}] ${r.reminder}`).join('\n'),
+      footer: { text: 'Send the number of the reminder you want me to forget (e.g. 2), or send c to cancel.' }
+    }});
+
+  const fetchAndParseInput = async () => {
+    // eslint-disable-next-line prefer-const
+    let [message, reason] = await msg.channel.awaitMessages(this.client, m => m.author.id === msg.author.id, { maxMatches: 1, time: 45000 });
+    if (!message[0] && reason === 'time') {
+      this.sendMessage(msg.channel.id, 'Prompt timed out.');
+      return false;
+    }
+
+    message = message[0].content.toLowerCase();
+    if (message.includes('delete') || message.includes('forget')) {
+      return false;
+    }
+    if (message === 'c' || message === 'cancel') {
+      this.sendMessage(msg.channel.id, 'Cancelled.');
+      return false;
+    }
+
+    if (!parseInt(message)) {
+      this.sendMessage(msg.channel.id, 'Send the number of the reminder you want me to forget (e.g. `2`), or send `c` to cancel.');
+      return fetchAndParseInput();
+    }
+    if (parseInt(message) > reminders.length) {
+      this.sendMessage(msg.channel.id, 'You don\'t have that many reminders. Send the number of the reminder you want me to forget (e.g. `3`), or send `c` to cancel.');
+      return fetchAndParseInput();
+    }
+    return parseInt(message);
+  };
+
+  const choice = await fetchAndParseInput();
+  if (choice) {
+    const r = reminders[choice - 1];
+    await this.db.deleteReminder(r._id);
+    return `Reminder \`${r.reminder}\` deleted successfully.`;
+  }
+}
+
+module.exports = {
+  call: forgetCommand,
+  name: 'forget',
+  aliases: ['delete'],
+  description: 'Starts a guided tour to forget one of your reminders.'
+};
---
+++ src/bot/commands/list.js
@@ -0,0 +1,36 @@
+const humanize = require('humanize-duration');
+
+async function listCommand (msg) {
+  const reminders = await this.db.getReminders(msg.author.id);
+  if (!reminders[0]) {
+    return 'You have no reminders set.';
+  }
+
+  const embed = {
+    title: 'Current reminder(s):',
+    fields: []
+  };
+
+  for (const reminder of reminders) {
+    if (embed.fields.length < 25) {
+      embed.fields.push({
+        name: `Due in ${humanize(reminder.dueDate - Date.now(), { conjunction: ' and ', serialComma: false, largest: 3, round: true })}`,
+        value: reminder.reminder
+      });
+    }
+  }
+
+  const sent = await this.sendMessage(msg.author.id, { embed }, true);
+  if (!sent) {
+    return { embed };
+  } else {
+    return ':ballot_box_with_check: Check your DMs!';
+  }
+}
+
+module.exports = {
+  call: listCommand,
+  name: 'list',
+  aliases: ['reminders'],
+  description: 'Sends you your current reminders in DM. Note: will send in channel if DMs are disabled.'
+};
