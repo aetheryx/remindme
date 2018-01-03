d9ee42bfb734362ad7ff951cce4d152d7198c7bb
aetheryx <zain1@live.nl>
2018-01-03 02:25:36 +0100

    updated to use new format

--- src/bot/commands/stats.js
+++ src/bot/commands/stats.js
@@ -10,26 +10,24 @@ async function statsCommand (msg) {
     this.client.shards.get(0);
 
   return {
-    embed: {
-      title: `RemindMeBot ${botVersion}`,
-      url: 'http://remindmebot.xyz',
-      fields: [
-        { name: 'Guilds', value: this.client.guilds.size, inline: true },
-        { name: 'Uptime', value: this.utils.parseDuration(process.uptime()), inline: true },
-        { name: 'Ping', value: `${shard.latency.toFixed()} ms`, inline: true },
-        { name: 'RAM Usage', value: `${(memUsage / 1048576).toFixed()}MB/${(totalMem / 1073741824).toFixed(1)} GB\n(${(memUsage / totalMem * 100).toFixed(2)}%)`, inline: true
-        },
-        { name: 'System Info', value: `${process.platform} (${process.arch})\n${(os.totalmem() > 1073741824 ? `${(os.totalmem() / 1073741824).toFixed(1)} GB` : `${(os.totalmem() / 1048576).toFixed(2)} MB`)}`, inline: true },
-        { name: 'Libraries', value: `[Eris](https://abal.moe/Eris) v${erisVersion}\n[Node.js](https://nodejs.org/en/) ${process.version}`, inline: true },
-        { name: 'Links', value: [
-          `[Bot invite](https://discordapp.com/oauth2/authorize?permissions=27648&scope=bot&client_id=${this.client.user.id})`,
-          '[Support server invite](https://discord.gg/Yphr6WG)',
-          '[GitHub](https://github.com/Aetheryx/remindme)',
-          '[Website](http://remindmebot.xyz)'
-        ].join(' | ') },
-      ],
-      footer: { text: 'Created by Aetheryx#2222' }
-    }
+    title: `RemindMeBot ${botVersion}`,
+    url: 'http://remindmebot.xyz',
+    fields: [
+      { name: 'Guilds', value: this.client.guilds.size, inline: true },
+      { name: 'Uptime', value: this.utils.parseDuration(process.uptime()), inline: true },
+      { name: 'Ping', value: `${shard.latency.toFixed()} ms`, inline: true },
+      { name: 'RAM Usage', value: `${(memUsage / 1048576).toFixed()}MB/${(totalMem / 1073741824).toFixed(1)} GB\n(${(memUsage / totalMem * 100).toFixed(2)}%)`, inline: true
+      },
+      { name: 'System Info', value: `${process.platform} (${process.arch})\n${(totalMem > 1073741824 ? `${(totalMem / 1073741824).toFixed(1)} GB` : `${(totalMem / 1048576).toFixed(2)} MB`)}`, inline: true },
+      { name: 'Libraries', value: `[Eris](https://abal.moe/Eris) v${erisVersion}\n[Node.js](https://nodejs.org/en/) ${process.version}`, inline: true },
+      { name: 'Links', value: [
+        `[Bot invite](https://discordapp.com/oauth2/authorize?permissions=27648&scope=bot&client_id=${this.client.user.id})`,
+        '[Support server invite](https://discord.gg/Yphr6WG)',
+        '[GitHub](https://github.com/Aetheryx/remindme)',
+        '[Website](http://remindmebot.xyz)'
+      ].join(' | ') },
+    ],
+    footer: { text: 'Created by Aetheryx#2222' }
   };
 }
 
